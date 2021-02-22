717282e26a9aea3f7a601b70ac11617665098b0b
ROLDAN AQUINO SEGURA <roldanaquinosegura@gmail.com>
2021-02-22 10:54:58 -0600

    estructuras de control

---
+++ hola.php
@@ -0,0 +1,5 @@
+<?php  
+
+	print "Hola archivo no index";
+
+?>
--- index.php
+++ index.php
@@ -1,6 +1,47 @@
-<?php  
+<?php
+	# Con echo mostramos mensajes en pantalla  
+	//echo 'Hola mundo';
 
-	echo 'Hola mundo';
+	//Uso de variables en php
+	//para declarar una variable en php se requiere el signo de pesos
+	//y no es necesario poner un tipo de dato
+	
+	$nombreVariable = "";
+	$nombreVariable2 = 2;
+	$nombreVariable3 = 1.2;
+	$nombreVariable4 = true; //false
 
 
+	//signos de operaciones basicas 
+	// + para sumar
+	// - para restar
+	// * para multiplicar
+	// / para dividir 
+
+	$valor1 = 5;
+	$valor2 = 3;
+	$resultado = $valor1 + $valor2;
+
+	//echo $resultado;
+
+
+	//concatenacion con php
+
+	$palabra1 = "Programacion ";
+	$palabra2 = "PHP";
+
+	//echo $palabra1 . $palabra2;
+
+
+	//estructura de control if 
+
+	if (!true) {
+		//echo "El if funciona";
+	}
+
+	if (false) {
+		
+	} else {
+		//echo "El if es falso";
+	}
 ?>
