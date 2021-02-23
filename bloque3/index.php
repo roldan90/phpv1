a0ec41028870580dd85aa7c4ace0d7953e9250ed
ROLDAN AQUINO SEGURA <roldanaquinosegura@gmail.com>
2021-02-23 09:40:25 -0600

    empezando el bloque 2

---
+++ bloque3/index.php
@@ -0,0 +1,19 @@
+
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<form method="GET" action="#">
+			<label>Valor 1</label>
+			<input type="text" name="valor1">
+			<button>Enviar</button>
+		</form>
+	</body>
+</html>
+
+<!--
+	Metodos de envio de datos 
+	POST: Se envia al servidor
+	GET: se envia al cliente 
+-->
