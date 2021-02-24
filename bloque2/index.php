8ca1e2994633bbabd872d855593228ab553ae85a
ROLDAN AQUINO SEGURA <roldanaquinosegura@gmail.com>
2021-02-24 09:58:23 -0600

    agregado de tipo de metodos y retornos

--- bloque2/index.php
+++ bloque2/index.php
@@ -1,19 +1,59 @@
 <?php 
 
 	//crear una clase co PHP
-
-	class MiClase {
+	
+	class MiClase2 {
 		public function miMetodo() {
-			return "Hola";
+			return "Vengo desde miclase2";
 		}
 	}
 
+	class MiClase extends MiClase2 {
+		//retornando un string
+		public $usuario = "Administrador";
+
+		public function llenaMetodo() {
+			//self se usa para llamar a un metodo dentro de otro de manera estatica
+			return self::miMetodo();
+		}
+
+		public function llenadoDos() {
+			$miMetodoClase1 = self::miMetodo();
+			$miMetodoClase2 = parent::miMetodo();
+
+			return $miMetodoClase1 . " " . $miMetodoClase2;
+		}
+
+		public function miMetodo() {
+			return $this->usuario;
+		}
+
+		//retornando un entero
+		public function metodoEntero() {
+			return 1;
+		}
+
+		//retornado de flotante
+		public function metodoFloat() {
+			return 5.5;
+		}
+
+		//retornado de boolean
+		public function metodoBool() {
+			return true;
+		}
+
+		//retornado de arreglo
+		public function metodoArreglo() {
+			$arreglo = array(1,2);
+			return $arreglo;
+		}
+	}
+//******************************************************************
 	//como crear un objeto en php
-
 	$MiClase = new MiClase();
-
 	//como llamo al metodo en php
+	print_r($MiClase->llenadoDos());
 
-	echo $MiClase->miMetodo();
-
+	//funcionaes anonimas y promesas en relacion a php
  ?>
