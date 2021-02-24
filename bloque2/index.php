e3427d6b744903bb85f82fc1580f65917e5f3614
ROLDAN AQUINO SEGURA <roldanaquinosegura@gmail.com>
2021-02-24 09:41:19 -0600

    agregado de bloque 2 y primer clase

---
+++ bloque2/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+	//crear una clase co PHP
+
+	class MiClase {
+		public function miMetodo() {
+			return "Hola";
+		}
+	}
+
+	//como crear un objeto en php
+
+	$MiClase = new MiClase();
+
+	//como llamo al metodo en php
+
+	echo $MiClase->miMetodo();
+
+ ?>
