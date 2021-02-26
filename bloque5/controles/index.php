a9d7db84dafe1146becc2efa6f73dc9b3ca23c5d
ROLDAN AQUINO SEGURA <roldanaquinosegura@gmail.com>
2021-02-26 11:41:34 -0600

    agregado de select

--- bloque5/controles/index.php
+++ bloque5/controles/index.php
@@ -3,10 +3,19 @@
 <head>
 	<title>Controles html y js ajax</title>
 	<script src="jquery-3.5.1.min.js"></script>
+	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </head>
 <body>
 	<h2>Controles de formulario con jquery</h2>
 	<form id="frmValores">
+		<label>Selecciona operacion</label>
+		<select id="opcion" name="opcion">
+			<option value="1">Suma</option>
+			<option value="2">Resta</option>
+			<option value="3">Multiplicacion</option>
+			<option value="4">Division</option>
+		</select>
+		<br>
 		<label>Valor1</label>
 		<input type="text" name="valor1" id="valor1">
 		<br>
@@ -19,10 +28,34 @@
 	<script type="text/javascript">
 		$(document).ready(function(){
 			$('#btnEnviar').click(function(){
+				/*let valor1 = $('#valor1').val();
+				valor1 = parseInt(valor1);
+				let valor2 = parseInt($('#valor2').val());
+				console.log(valor1 + valor2);
+				return false;*/
+
+				let opcion = $('#opcion').val();
 				let valor1 = parseInt($('#valor1').val());
 				let valor2 = parseInt($('#valor2').val());
+				let resultado = 0;
+				switch(opcion) {
+					case "1":
+						resultado = valor1 + valor2;
+						break;
+					case "2":
+						resultado = valor1 - valor2;
+						break;
+					case "3":
+						resultado = valor1 * valor2;
+						break;
+					case "4":
+						resultado = valor1 / valor2;
+						break;
+				}
 
-				console.log(valor1 + valor2);
+				//swal("El resultado es " + resultado);
+				swal(":D","El resultado es " + resultado,"success");
+
 				return false;
 			});
 		});
